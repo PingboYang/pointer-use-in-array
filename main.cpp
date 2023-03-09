29fceb398692789dd6e1cce03421a3c51355c788
Pingbo Yang <86988793+PingboYang@users.noreply.github.com>
2023-03-09 18:20:38 +0000

    add function for printing the array in a different way

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int ar[],int size);
+void printArrayPointers1(int ar[],int size);
+
 int main() {
+  const int size=4;
   int ar[]={10,20,30,15};
+  cout<<"\nPrint array using subscripts";
+  printArray(ar,size);
+    cout<<"\n----------------";
+  cout<<"\nPrint array using address";
+  printArrayPointers1(ar,size);
   int *pA;
   pA=ar;
   pA++;
@@ -29,4 +38,15 @@ int main() {
     cout<<"\n"<<ar[i];
   }
 
+}
+void printArray(int ar[],int size){
+  for(int i=0;i<size;i++){
+    cout<<"\n"<<ar[i];
+  }
+}
+void printArrayPointers1(int ar[],int size){
+  for (int i=0; i<size;i++){
+    cout<<"\n"<<*(ar+i);
+    cout<<"\n"<<ar+1;
+  }
 }
